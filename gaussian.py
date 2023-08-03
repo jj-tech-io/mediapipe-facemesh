f07870dfc06b89ad2a1da9889f317c275be3223e
Joel Johnson <60250054+jj-tech-io@users.noreply.github.com>
2023-08-03 23:13:44 +0200

    florence

--- gaussian.py
+++ gaussian.py
@@ -1,6 +1,8 @@
+#%
 import numpy as np
 import matplotlib.pyplot as plt
-
+import cv2
+""" 
 # Image size
 size = (512, 512)
 
@@ -25,18 +27,32 @@ for x in range(size[0]):
 # Show the image
 plt.imshow(image, cmap='gray', vmin=0, vmax=1)
 plt.show()
+"""
+Cm_path = r"images/m141_Cm.png"
+Ch_path = r"images/m141_Ch.png"
 
-#create function to generate gaussian
-def gaussian(size, sigma, intensity):
-    # Create an empty image
-    image = np.zeros(size)
+#load Cm and Ch as binary images
+Cm = plt.imread(Cm_path).astype(np.float32)
+Ch = plt.imread(Ch_path).astype(np.float32)
+#resize to 512x512
+Cm = cv2.resize(Cm, (512, 512))
+Ch = cv2.resize(Ch, (512, 512))
 
-    # Center of the blemish
-    cx, cy = size[0] // 2, size[1] // 2
-
-    # Create the blemish
-    for x in range(size[0]):
-        for y in range(size[1]):
+def gaussian(image, cx, cy, sigma, intensity):
+    modified_image = image.copy()
+    rows, cols, channels = modified_image.shape
+    for x in range(rows):
+        for y in range(cols):
             dist = np.sqrt((x - cx)**2 + (y - cy)**2)
-            image[x, y] += intensity * np.exp(-(dist**2) / (2 * sigma**2))
-    return image
+            modified_image[x, y] += intensity * np.exp(-(dist**2) / (2 * sigma**2))
+            # Clip the values to make sure they remain within the valid range
+            modified_image[x, y] = np.clip(modified_image[x, y], 0, 1)
+    return modified_image
+
+
+gaus = gaussian(Cm, 250, 150, 2, 1.0)
+
+#%
+plt.imshow(gaus, cmap='binary', vmin=0, vmax=1)
+plt.show()
+# %
